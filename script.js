96ee86a63f2a3d36f0e4d459fa1ad96faa350b26
agarwal002 <113688959+agarwal002@users.noreply.github.com>
2023-12-16 10:24:55 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,31 @@
+
+let player;
+
+function onYouTubeIframeAPIReady() {
+    player = new YT.Player('player', {
+        height: '360',
+        width: '640',
+        playerVars: {
+            'autoplay': 0,
+            'controls': 1,
+            'showinfo': 0,
+            'rel': 0,
+            'key': 'AIzaSyCRnmDZb1HUgDqrcepMksW5SVPulLPok0A',
+        },
+    });
+}
+
+function loadVideo() {
+    const videoId = document.getElementById('videoIdInput').value;
+    if (videoId.trim() !== '') {
+        if (player) {
+            player.loadVideoById(videoId);
+        } else {
+            setTimeout(() => {
+                loadVideo();
+            }, 1000);
+        }
+    } else {
+        alert('Please enter a valid YouTube Video ID.');
+    }
+}
